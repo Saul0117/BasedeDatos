57be18e70e62bb7c12a51f8f3a47bcd2744c5db7
Probiazº <alexy-tobiaz2002@hotmail.com>
2019-11-06 12:28:13 -0600

    xd

---
+++ BASEDEDATOS/BASEDEDATOS/Principal.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BASEDEDATOS.Clase;
+using SQLite;
+
+namespace BASEDEDATOS
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+    public partial class Principal : Window
+    {
+        List<Contactos> contactos;
+        public Principal()
+        {
+            InitializeComponent();
+            contactos = new List<Contactos>();
+            LeerBaseDatos();
+        }
+        void LeerBaseDatos()
+        {
+            using (SQLite.SQLiteConnection conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contactos>();
+                contactos = (conn.Table<Contactos>().ToList()).OrderBy(c => c.Nombre).ToList();
+            }
+            if(contactos !=null)
+            {
+                lvContactos.ItemsSource = contactos;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BASEDEDATOS.MainWindow form = new BASEDEDATOS.MainWindow();
+            form.ShowDialog();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
